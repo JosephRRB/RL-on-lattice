2dc2fe50a151b11c82cd88467e87edad43e50cc2
josephb <josephb@stradigi.ai>
2022-01-28 15:24:42 -0500

    added initial test

---
+++ tests/integration_test.py
@@ -0,0 +1,8 @@
+from core.agent import RLAgent
+from core.environment import KagomeLatticeEnv
+
+if __name__ == '__main__':
+    env = KagomeLatticeEnv(n_sq_cells=2)
+    agent = RLAgent(env, n_hidden=10, learning_rate=0.0005)
+
+    losses = agent.learn(run_n_trajectories=2, train_n_epochs=2)
