f214225e92f44651f5c3ff1b319c979db6ba3028
josephb <jrrbunao721@gmail.com>
2022-02-24 15:34:26 -0500

    training step

--- core/runner.py
+++ core/runner.py
@@ -10,7 +10,7 @@ class Runner:
         self.environment = environment
         self.agent = agent
 
-        self.agent._batch_graphs(n_batch=self.n_transitions)
+        self.agent.create_batched_graphs(n_batch=self.n_transitions)
         self.current_state = environment.reset()
 
     def run_trajectory(self):
@@ -38,8 +38,36 @@ class Runner:
 
         return old_obs, actions, new_obs, rewards
 
-    def _train_on_one_trajectory(self):
+    def _training_step(self):
         old_obs, actions, new_obs, rewards = self.run_trajectory()
+
+        old_log_probas = self.environment.calculate_log_probas_of_spin_states(old_obs)
+        new_log_probas = self.environment.calculate_log_probas_of_spin_states(new_obs)
+        delta_log_probas = new_log_probas - old_log_probas
+
+        with tf.GradientTape() as tape:
+            forward_log_probas = self.agent.calculate_log_probas_of_agent_actions(old_obs, actions)
+            # Same action would map back the state
+            backward_log_probas = self.agent.calculate_log_probas_of_agent_actions(new_obs, actions)
+
+            bidir_acceptance_log_probas = -tf.abs(
+                delta_log_probas + backward_log_probas - forward_log_probas
+            ) / 2
+            log_probs = bidir_acceptance_log_probas + (
+                        forward_log_probas + backward_log_probas) / 2
+            grad_weights_logits = tf.stop_gradient(
+                bidir_acceptance_log_probas + tf.math.log(rewards)
+            )
+            grad_weights = tf.nn.softmax(grad_weights_logits, axis=0)
+            # theoretical ave of grad_weights
+            baseline = 1 / self.n_transitions
+            advantages = grad_weights - baseline
+
+            # negative log prob -> gradient descent
+            loss = tf.reduce_sum(-log_probs * advantages)
+
+        grads = tape.gradient(loss, self.agent.policy_network.trainable_weights)
+        self.agent.optimizer.apply_gradients(zip(grads, self.agent.policy_network.trainable_weights))
 
 
 
