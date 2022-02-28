0fe048953d5f45aff3a2282e67b656cf4c4c67ed
josephb <jrrbunao721@gmail.com>
2022-02-28 17:39:28 -0500

    expts to redefine agent actions

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -111,7 +111,7 @@ def test_evaluate_does_not_change_policy_network_weights():
 
 
 def test_example():
-    lattice = KagomeLattice(n_sq_cells=2).lattice
+    lattice = KagomeLattice(n_sq_cells=3).lattice
     environment = SpinEnvironment(
         lattice,
         inverse_temp=0.5,
@@ -131,7 +131,197 @@ def test_example():
         evaluate_after_n_training_steps=100, evaluate_for_n_transitions=50
     )
 
-    np.savetxt("data/example_training_result.csv", train_ave_rewards.numpy(), delimiter=",")
+    np.savetxt("data/example_training_result_1.csv", train_ave_rewards.numpy(), delimiter=",")
 
     plt.plot(train_ave_rewards)
-    plt.savefig("data/sample_training_plot.png")
+    plt.savefig("data/sample_training_plot_1.png")
+
+
+def test():
+    from collections import Counter
+    n_expts = 1000000
+    rng = np.random.default_rng()
+    selected = []
+    for i in range(n_expts):
+        select = np.sort(rng.choice([0, 1, 2], size=2, replace=False, p=[0.5, 0.25, 0.25]))
+        # select = np.sort(rng.choice([0, 1, 2], size=1, replace=False, p=[0.5, 0.25, 0.25]))
+
+        # select = np.sort(rng.choice([0, 1], size=1, replace=False, p=[0.6, 0.4]))
+        selected.append(str(select))
+        if i % 1000 == 0:
+            counts_ = Counter(selected)
+            n_ = sum(counts_.values())
+            freq_ = {k: v / n_ for k, v in counts_.items()}
+            print(freq_)
+        # selected.append(select.reshape((1, 2)))
+    # selected = np.concatenate(selected, axis=0)
+    counts = Counter(selected)
+    n = sum(counts.values())
+    freq = {k: v/n for k, v in counts.items()}
+    print(freq)
+
+
+def test2():
+    "with replacement"
+    p = tf.constant([[0.5, 0.25, 0.25]], dtype=tf.float32)
+    n_expts = 1000
+    logits = tf.concat([tf.math.log(p)]*n_expts, axis=0)
+    select = tf.random.categorical(logits, 2)
+
+    print(select)
+
+# https://timvieira.github.io/blog/post/2014/07/31/gumbel-max-trick/
+# https://lips.cs.princeton.edu/the-gumbel-max-trick-for-discrete-distributions/
+# https://github.com/tensorflow/tensorflow/issues/9260
+# http://timvieira.github.io/blog/post/2014/08/01/gumbel-max-trick-and-weighted-reservoir-sampling/
+# https://arxiv.org/abs/1903.06059
+# https://arxiv.org/abs/1901.10517
+def test3():
+    from collections import Counter
+    p = tf.constant([0.5, 0.35, 0.15], dtype=tf.float32)
+    n_expts = 100000
+    logits = tf.concat([tf.reshape(tf.math.log(p), shape=(1, -1))] * n_expts, axis=0)
+    # ------------------------------------------------
+
+    pick_k = 2
+    # pick_k = 3
+    uniform = tf.random.uniform(shape=logits.shape, minval=0, maxval=1)
+    gumbel = -tf.math.log(-tf.math.log(uniform))
+    _, indices = tf.nn.top_k(logits + gumbel, pick_k)
+    # ------------------------------------------------
+
+    selected = indices.numpy()
+    selected = [str(s) for s in selected]
+    counts = Counter(selected)
+    n = sum(counts.values())
+    freq = {k: v / n for k, v in counts.items()}
+
+    # ------------------------------------------------
+    unique_indices = tf.constant([
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 2],
+        [2, 0],
+        [2, 1]
+    ])
+    # unique_indices = tf.constant([
+    #     [0, 1, 2],
+    #     [0, 2, 1],
+    #     [1, 0, 2],
+    #     [1, 2, 0],
+    #     [2, 0, 1],
+    #     [2, 1, 0]
+    # ])
+    selected_p = tf.gather(params=p, indices=unique_indices)
+    renorm_probs = 1 - tf.cumsum(selected_p[:, :-1], axis=1)
+    prob_seq = tf.reduce_prod(selected_p, axis=1, keepdims=True) / tf.reduce_prod(renorm_probs, axis=1, keepdims=True)
+    # ------------------------------------------------
+
+    unique_indices = unique_indices.numpy()
+    unique_indices = [str(i) for i in unique_indices]
+    prob_seq = prob_seq.numpy()
+
+    expected = {
+        k: v[0] for k, v in zip(unique_indices, prob_seq)
+    }
+
+    # expected = {
+    #     '[0 1]': 0.5*0.35/(1-0.5),
+    #     '[0 2]': 0.5*0.15/(1-0.5),
+    #     '[1 0]': 0.35*0.5/(1-0.35),
+    #     '[1 2]': 0.35 * 0.15 / (1 - 0.35),
+    #     '[2 0]': 0.15 * 0.5 / (1 - 0.15),
+    #     '[2 1]': 0.15 * 0.35 / (1 - 0.15),
+    # }
+    tol = 1e-2
+    assert all([np.abs(freq[i] - expected[i]) < tol for i in unique_indices])
+
+
+
+def test4():
+    selected = tf.constant([
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 2],
+        [2, 0],
+        [2, 1]
+    ])
+    p = tf.constant([[0.5], [0.35], [0.15]], dtype=tf.float32)
+    selected_p = tf.gather(params=tf.reshape(p, shape=(-1,)), indices=selected)
+    renorm_probs = 1 - tf.cumsum(selected_p[:, :-1], axis=1)
+
+    prob_seq = tf.reduce_prod(selected_p, axis=1, keepdims=True)/tf.reduce_prod(renorm_probs, axis=1, keepdims=True)
+
+    print(prob_seq)
+    # print(selected_p)
+    # print(renorm_probs)
+
+
+def test5():
+    from collections import Counter
+
+    n_expts = 1000000
+    p = tf.constant([0.5, 0.35, 0.15], dtype=tf.float32)
+    logits = tf.concat([tf.reshape(tf.math.log(p), shape=(1, -1))] * n_expts, axis=0)
+
+    # --------------
+
+    uniform = tf.random.uniform(shape=logits.shape, minval=0, maxval=1)
+    gumbel = -tf.math.log(-tf.math.log(uniform))
+    noisy_logits = tf.split(logits + gumbel, n_expts)
+
+    n_nodes = 3
+    pick_k = np.random.randint(low=1, high=n_nodes+1, size=n_expts)
+    selected_inds = []
+    for k, n in zip(pick_k, noisy_logits):
+        _, indices = tf.nn.top_k(n, k)
+        selected_inds.append(indices)
+    #     selected_inds.append(tf.RaggedTensor.from_tensor(indices))
+    # tf_selected_inds = tf.concat(selected_inds, axis=0)
+
+    # --------------
+
+    selected = [str(s.numpy()[0]) for s in selected_inds]
+    counts = Counter(selected)
+    n = sum(counts.values())
+    freq = {k: v / n for k, v in counts.items()}
+
+    # --------------
+    unique_indices = tf.ragged.constant([
+        [0],
+        [1],
+        [2],
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 2],
+        [2, 0],
+        [2, 1],
+        [0, 1, 2],
+        [0, 2, 1],
+        [1, 0, 2],
+        [1, 2, 0],
+        [2, 0, 1],
+        [2, 1, 0],
+    ])
+    selected_p = tf.gather(params=p, indices=unique_indices)
+    renorm_probs = 1 - tf.map_fn(lambda x: tf.cumsum(x[:-1]), selected_p)
+    prob_seq = tf.reduce_prod(selected_p, axis=1, keepdims=True) / tf.reduce_prod(renorm_probs, axis=1, keepdims=True) / n_nodes
+
+    # ------------------------------------------------
+
+    unique_indices = [str(i.numpy()) for i in unique_indices]
+    prob_seq = prob_seq.numpy().ravel()
+
+    expected = {
+        k: v for k, v in zip(unique_indices, prob_seq)
+    }
+
+    results = {
+        ui: (freq[ui], expected[ui]) for ui in unique_indices
+    }
+    tol = 1e-2
+    assert all([np.abs(f - e) < tol for f, e in results.values()])
+    print(results)
