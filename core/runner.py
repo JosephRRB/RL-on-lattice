cda5c1c9062650e084734175667f3a4340c8dfe7
JosephRRB <jrrbunao721@gmail.com>
2022-03-05 14:50:16 -0500

    e2e tests

--- core/runner.py
+++ core/runner.py
@@ -43,18 +43,31 @@ class Runner:
 
         with tf.GradientTape() as tape:
             (
-                log_probas_for_loss,
-                bidir_acceptance_log_probas,
+                delta_log_probas,
+                forward_log_probas,
+                backward_log_probas,
             ) = self._calculate_log_probas(
                 self.batched_graphs_for_training, old_obs, actions, new_obs
+            )
+            bidir_acceptance_log_probas = (
+                -tf.abs(
+                    delta_log_probas + backward_log_probas - forward_log_probas
+                )
+                / 2
+            )
+            log_probas_for_loss = (
+                bidir_acceptance_log_probas
+                + (forward_log_probas + backward_log_probas) / 2
             )
 
             grad_weights_logits = tf.stop_gradient(
                 bidir_acceptance_log_probas + tf.math.log(rewards)
             )
             grad_weights = tf.nn.softmax(grad_weights_logits, axis=0)
-            # theoretical ave of grad_weights
-            baseline = 1 / n_transitions
+            # # theoretical ave of grad_weights
+            # baseline = 1 / n_transitions
+
+            baseline = tf.reduce_mean(grad_weights, axis=0, keepdims=True)
             advantages = grad_weights - baseline
 
             # negative log prob -> gradient descent
@@ -84,18 +97,7 @@ class Runner:
         backward_log_probas = self.agent.calculate_log_probas_of_agent_actions(
             graphs, new_obs, reversed_actions
         )
-
-        bidir_acceptance_log_probas = (
-            -tf.abs(
-                delta_log_probas + backward_log_probas - forward_log_probas
-            )
-            / 2
-        )
-        log_probas_for_loss = (
-            bidir_acceptance_log_probas
-            + (forward_log_probas + backward_log_probas) / 2
-        )
-        return log_probas_for_loss, bidir_acceptance_log_probas
+        return delta_log_probas, forward_log_probas, backward_log_probas
 
     def train(
         self,
@@ -110,28 +112,45 @@ class Runner:
         self.batched_graphs_for_evaluation = _create_batched_graphs(
             self.agent.graph, n_batch=evaluate_for_n_transitions
         )
-        train_ave_rewards = []
+        train_acc_rate = []
         for i in range(n_training_loops):
             self._training_step(n_transitions=n_transitions_per_training_step)
             if i % evaluate_after_n_training_steps == 0:
                 # print(f"Training step #: {i+1}")
-                ave_reward = self._evaluate(
+                acc_rate = self._evaluate(
                     evaluate_for_n_transitions=evaluate_for_n_transitions
                 )
-                train_ave_rewards.append(ave_reward)
+                train_acc_rate.append(acc_rate)
 
-        train_ave_rewards = tf.concat(train_ave_rewards, axis=0)
-        return train_ave_rewards
+        train_acc_rate = tf.concat(train_acc_rate, axis=0)
+        return train_acc_rate
 
     def _evaluate(self, evaluate_for_n_transitions=100):
         "Batch graphs before running _evaluate"
         old_obs, actions, new_obs, rewards = self.run_trajectory(
             n_transitions=evaluate_for_n_transitions
         )
-        _, bidir_acc_log_probas = self._calculate_log_probas(
+        (
+            delta_log_probas,
+            forward_log_probas,
+            backward_log_probas,
+        ) = self._calculate_log_probas(
             self.batched_graphs_for_evaluation, old_obs, actions, new_obs
         )
-        ave_reward = tf.reduce_mean(
-            tf.math.exp(bidir_acc_log_probas) * rewards
+        acceptance_lp = tf.math.minimum(
+            0, delta_log_probas + backward_log_probas - forward_log_probas
         )
-        return ave_reward
+        acceptance_prob = tf.math.exp(acceptance_lp)
+        rand = tf.random.uniform(shape=acceptance_prob.shape)
+        n_accepted = tf.reduce_sum(
+            tf.cast(rand < acceptance_prob, dtype=tf.float32),
+            axis=0,
+            keepdims=True,
+        )
+        acceptance_rate = n_accepted / acceptance_prob.shape[0]
+
+        return acceptance_rate
+        # ave_reward = tf.reduce_mean(
+        #     tf.math.exp(bidir_acc_log_probas) * rewards
+        # )
+        # return ave_reward
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -113,178 +113,175 @@ def test_evaluate_does_not_change_policy_network_weights():
         tf.debugging.assert_equal(w0, w1)
 
 
-#
-#
-# def test_example():
-#     lattice = KagomeLattice(n_sq_cells=3).lattice
-#     environment = SpinEnvironment(
-#         lattice,
-#         inverse_temp=0.5,
-#         spin_coupling=0.5,
-#         external_B=0
-#     )
-#     agent = RLAgent(
-#         lattice,
-#         n_hidden=10,
-#         learning_rate=0.0005
-#     )
-#
-#     runner = Runner(environment, agent)
-#
-#     train_ave_rewards = runner.train(
-#         n_training_loops=10000, n_transitions_per_training_step=2,
-#         evaluate_after_n_training_steps=100, evaluate_for_n_transitions=50
-#     )
-#
-#     np.savetxt("data/example_training_result_1.csv", train_ave_rewards.numpy(), delimiter=",")
-#
-#     plt.plot(train_ave_rewards)
-#     plt.savefig("data/sample_training_plot_1.png")
+def test_example():
+    lattice = KagomeLattice(n_sq_cells=2).lattice
+    environment = SpinEnvironment(
+        lattice, inverse_temp=0.5, spin_coupling=0.5, external_B=0
+    )
+    agent = RLAgent(lattice, n_hidden=10, learning_rate=0.0001)
 
+    runner = Runner(environment, agent)
 
-def test5():
-    from collections import Counter
-
-    n_expts = 100000
-    p = tf.constant([0.5, 0.35, 0.15], dtype=tf.float32)
-    logits = tf.concat(
-        [tf.reshape(tf.math.log(p), shape=(1, -1))] * n_expts, axis=0
+    train_acc_rate = runner.train(
+        n_training_loops=10000,
+        n_transitions_per_training_step=2,
+        evaluate_after_n_training_steps=100,
+        evaluate_for_n_transitions=100,
     )
 
-    # --------------
-
-    uniform = tf.random.uniform(shape=logits.shape, minval=0, maxval=1)
-    gumbel = -tf.math.log(-tf.math.log(uniform))
-    noisy_logits = tf.split(logits + gumbel, n_expts)
-
-    # n_nodes = 3
-    # pick_k = np.random.randint(low=1, high=n_nodes + 1, size=n_expts)
-    n_probs = np.array([0.3, 0.6, 0.1])
-    pick_k = np.random.choice([1, 2, 3], p=n_probs, size=n_expts)
-    selected_inds = []
-    for k, n in zip(pick_k, noisy_logits):
-        _, indices = tf.nn.top_k(n, k)
-        selected_inds.append(indices)
-    #     selected_inds.append(tf.RaggedTensor.from_tensor(indices))
-    # tf_selected_inds = tf.concat(selected_inds, axis=0)
-
-    # --------------
-
-    selected = [str(s.numpy()[0]) for s in selected_inds]
-    counts = Counter(selected)
-    n = sum(counts.values())
-    freq = {k: v / n for k, v in counts.items()}
-
-    # --------------
-    unique_indices = tf.ragged.constant(
-        [
-            [0],
-            [1],
-            [2],
-            [0, 1],
-            [0, 2],
-            [1, 0],
-            [1, 2],
-            [2, 0],
-            [2, 1],
-            [0, 1, 2],
-            [0, 2, 1],
-            [1, 0, 2],
-            [1, 2, 0],
-            [2, 0, 1],
-            [2, 1, 0],
-        ]
-    )
-    selected_p = tf.gather(params=p, indices=unique_indices)
-    renorm_probs = 1 - tf.map_fn(lambda x: tf.cumsum(x[:-1]), selected_p)
-    n_selected = unique_indices.row_lengths()
-    n_selected_probs = n_probs[n_selected.numpy() - 1].reshape((-1, 1))
-    prob_seq = (
-        tf.reduce_prod(selected_p, axis=1, keepdims=True)
-        / tf.reduce_prod(renorm_probs, axis=1, keepdims=True)
-        * n_selected_probs
+    np.savetxt(
+        "data/example_training_result.csv",
+        train_acc_rate.numpy(),
+        delimiter=",",
     )
 
-    # ------------------------------------------------
-
-    unique_indices = [str(i.numpy()) for i in unique_indices]
-    prob_seq = prob_seq.numpy().ravel()
-
-    expected = {k: v for k, v in zip(unique_indices, prob_seq)}
-
-    results = {ui: (freq[ui], expected[ui]) for ui in unique_indices}
-    tol = 1e-2
-    assert all([np.abs(f - e) < tol for f, e in results.values()])
-    print(results)
+    plt.plot(train_acc_rate)
+    plt.savefig("data/sample_training_plot.png")
 
 
-def test1():
-    params = tf.Variable([[2.0, 3.0], [1.0, 5.0]], dtype=tf.float32)
-    x = tf.constant([[2.0], [1.0]], dtype=tf.float32)
-    y = tf.constant([4.0, 2.0, 3.0, 1.0], dtype=tf.float32)
-
-    with tf.GradientTape() as tape:
-        u = tf.linalg.matmul(params, x)
-        v = tf.reshape(params, shape=(-1,))*y
-        w = tf.reduce_sum(u + tf.reduce_sum(v))
-    grad = tape.gradient(w, params)
-
-    expected_grad = tf.concat([tf.transpose(x), tf.transpose(x)], axis=0) + 2*tf.reshape(y, shape=(2, 2))
-
-    tf.debugging.assert_equal(grad, expected_grad)
-
-    res = tf.reduce_sum(expected_grad*params)
-
-    tf.debugging.assert_equal(w, res)
-    # param = [[a, b],
-    #          [c, d]]
-    # u = [[ax1 + bx2],
-    #      [cx1 + dx2]]
-    # v = [ay1, by2, cy3, dy4]
-    # w = ax1 + bx2 + cx1 + dx2 + 2(ay1 + by2 + cy3 + dy4)
-    #   = a(x1 + 2y1) + b(x2 + 2y2) + c(x1 + 2y3) + d(x2 + 2y4)
-
-
-def test2():
-    params = tf.Variable([[2.0, 3.0, 4.0],
-                          [1.0, 5.0, 3.0],
-                          [7.0, 6.0, 1.0]], dtype=tf.float32)
-    x = tf.ragged.constant([
-        [3.0, 2.0, 1.0],
-        [1.0],
-        [1.0, 4.0]
-    ])
-    z = tf.ragged.constant([
-        [2, 0, 1],
-        [1],
-        [0, 2]
-    ])
-    with tf.GradientTape() as tape:
-        u = tf.gather(params, z, axis=1, batch_dims=1)
-        v = tf.map_fn(lambda x: tf.cumsum(x[:-1]), u*x)
-        w = tf.reduce_sum(v)
-    grad = tape.gradient(w, params)
-
-    expected_grad = tf.constant([
-        [2.0, 0.0, 2*3.0],
-        [0.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0]
-    ])
-    tf.debugging.assert_equal(grad, expected_grad)
-
-    # params = [[a, b, c],
-    #           [d, e, f],
-    #           [g, h, i]]
-    # x = [[x00, x01, x02],
-    #      [x10],
-    #      [x20, x21]]
-
-    # u = [[c, a, b],
-    #      [e],
-    #      [g, i]]
-    # v = [[x00*c, x00*c + x01*a],
-    #      [],
-    #      [x20*g]]
-    # w = a*x01 + c*2*x00 + g*x20
-
-
+# def test5():
+#     from collections import Counter
+#
+#     n_expts = 100000
+#     p = tf.constant([0.5, 0.35, 0.15], dtype=tf.float32)
+#     logits = tf.concat(
+#         [tf.reshape(tf.math.log(p), shape=(1, -1))] * n_expts, axis=0
+#     )
+#
+#     # --------------
+#
+#     uniform = tf.random.uniform(shape=logits.shape, minval=0, maxval=1)
+#     gumbel = -tf.math.log(-tf.math.log(uniform))
+#     noisy_logits = tf.split(logits + gumbel, n_expts)
+#
+#     # n_nodes = 3
+#     # pick_k = np.random.randint(low=1, high=n_nodes + 1, size=n_expts)
+#     n_probs = np.array([0.3, 0.6, 0.1])
+#     pick_k = np.random.choice([1, 2, 3], p=n_probs, size=n_expts)
+#     selected_inds = []
+#     for k, n in zip(pick_k, noisy_logits):
+#         _, indices = tf.nn.top_k(n, k)
+#         selected_inds.append(indices)
+#     #     selected_inds.append(tf.RaggedTensor.from_tensor(indices))
+#     # tf_selected_inds = tf.concat(selected_inds, axis=0)
+#
+#     # --------------
+#
+#     selected = [str(s.numpy()[0]) for s in selected_inds]
+#     counts = Counter(selected)
+#     n = sum(counts.values())
+#     freq = {k: v / n for k, v in counts.items()}
+#
+#     # --------------
+#     unique_indices = tf.ragged.constant(
+#         [
+#             [0],
+#             [1],
+#             [2],
+#             [0, 1],
+#             [0, 2],
+#             [1, 0],
+#             [1, 2],
+#             [2, 0],
+#             [2, 1],
+#             [0, 1, 2],
+#             [0, 2, 1],
+#             [1, 0, 2],
+#             [1, 2, 0],
+#             [2, 0, 1],
+#             [2, 1, 0],
+#         ]
+#     )
+#     selected_p = tf.gather(params=p, indices=unique_indices)
+#     renorm_probs = 1 - tf.map_fn(lambda x: tf.cumsum(x[:-1]), selected_p)
+#     n_selected = unique_indices.row_lengths()
+#     n_selected_probs = n_probs[n_selected.numpy() - 1].reshape((-1, 1))
+#     prob_seq = (
+#         tf.reduce_prod(selected_p, axis=1, keepdims=True)
+#         / tf.reduce_prod(renorm_probs, axis=1, keepdims=True)
+#         * n_selected_probs
+#     )
+#
+#     # ------------------------------------------------
+#
+#     unique_indices = [str(i.numpy()) for i in unique_indices]
+#     prob_seq = prob_seq.numpy().ravel()
+#
+#     expected = {k: v for k, v in zip(unique_indices, prob_seq)}
+#
+#     results = {ui: (freq[ui], expected[ui]) for ui in unique_indices}
+#     tol = 1e-2
+#     assert all([np.abs(f - e) < tol for f, e in results.values()])
+#     print(results)
+#
+#
+# def test1():
+#     params = tf.Variable([[2.0, 3.0], [1.0, 5.0]], dtype=tf.float32)
+#     x = tf.constant([[2.0], [1.0]], dtype=tf.float32)
+#     y = tf.constant([4.0, 2.0, 3.0, 1.0], dtype=tf.float32)
+#
+#     with tf.GradientTape() as tape:
+#         u = tf.linalg.matmul(params, x)
+#         v = tf.reshape(params, shape=(-1,))*y
+#         w = tf.reduce_sum(u + tf.reduce_sum(v))
+#     grad = tape.gradient(w, params)
+#
+#     expected_grad = tf.concat([tf.transpose(x), tf.transpose(x)], axis=0) + 2*tf.reshape(y, shape=(2, 2))
+#
+#     tf.debugging.assert_equal(grad, expected_grad)
+#
+#     res = tf.reduce_sum(expected_grad*params)
+#
+#     tf.debugging.assert_equal(w, res)
+#     # param = [[a, b],
+#     #          [c, d]]
+#     # u = [[ax1 + bx2],
+#     #      [cx1 + dx2]]
+#     # v = [ay1, by2, cy3, dy4]
+#     # w = ax1 + bx2 + cx1 + dx2 + 2(ay1 + by2 + cy3 + dy4)
+#     #   = a(x1 + 2y1) + b(x2 + 2y2) + c(x1 + 2y3) + d(x2 + 2y4)
+#
+#
+# def test2():
+#     params = tf.Variable([[2.0, 3.0, 4.0],
+#                           [1.0, 5.0, 3.0],
+#                           [7.0, 6.0, 1.0]], dtype=tf.float32)
+#     x = tf.ragged.constant([
+#         [3.0, 2.0, 1.0],
+#         [1.0],
+#         [1.0, 4.0]
+#     ])
+#     z = tf.ragged.constant([
+#         [2, 0, 1],
+#         [1],
+#         [0, 2]
+#     ])
+#     with tf.GradientTape() as tape:
+#         u = tf.gather(params, z, axis=1, batch_dims=1)
+#         v = tf.map_fn(lambda x: tf.cumsum(x[:-1]), u*x)
+#         w = tf.reduce_sum(v)
+#     grad = tape.gradient(w, params)
+#
+#     expected_grad = tf.constant([
+#         [2.0, 0.0, 2*3.0],
+#         [0.0, 0.0, 0.0],
+#         [1.0, 0.0, 0.0]
+#     ])
+#     tf.debugging.assert_equal(grad, expected_grad)
+#
+#     # params = [[a, b, c],
+#     #           [d, e, f],
+#     #           [g, h, i]]
+#     # x = [[x00, x01, x02],
+#     #      [x10],
+#     #      [x20, x21]]
+#
+#     # u = [[c, a, b],
+#     #      [e],
+#     #      [g, i]]
+#     # v = [[x00*c, x00*c + x01*a],
+#     #      [],
+#     #      [x20*g]]
+#     # w = a*x01 + c*2*x00 + g*x20
+#
+#
